39443994ca5302c401c2f35cb50ce8ed5afbc835
timotheegoguely <timothee@goguely.com>
2021-10-18 21:16:58 +0200

    🩹 Add "top-0" and "fixed" classes on body when mobile nav is open

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,11 +5,14 @@ document.addEventListener('DOMContentLoaded', function() {
   toggleBtn.addEventListener('click', () => {
     let expanded = toggleBtn.getAttribute("aria-expanded") == "true" ? "false" : "true";
     toggleBtn.setAttribute("aria-expanded", expanded);
+    document.body.classList.toggle("top-0");
+    document.body.classList.toggle("fixed");
   })
 })
 
 window.addEventListener('resize', function() {
   if (window.innerWidth >= 768) {
     toggleBtn.setAttribute("aria-expanded", "false");
+    document.body.classList.remove("top-0", "fixed");
   }
 });
